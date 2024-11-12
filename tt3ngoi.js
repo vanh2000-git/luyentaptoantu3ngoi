04817433c1f0aa265633a191a09111a3eb8c828d
vanh2000-git <nguyenvietanh102000@gmail.com>
2024-11-12 11:27:39 +0700

    update

--- tt3ngoi.js
+++ tt3ngoi.js
@@ -1,4 +1,15 @@
+// Bai 1
 let a = prompt("a: ");
 let b = prompt("b: ");
 result = (a + b < 4) ? 'Below' : 'Over';
 document.write(result);
+// BAI 2
+let login = prompt("Enter:");
+let message = (login == 'Employee') ?
+    'Hello' :
+    (login == 'Director') ?
+        'Greetings' :
+        (login == '') ?
+            'No login' :
+            '';
+alert(message);
